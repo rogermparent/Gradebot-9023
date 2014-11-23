10db1eadadb44f84044cfb792c7afdf2c89a615c
rogermparent <rogermparent@gmail.com>
2014-11-22 19:45:34 -0500

    Switched to a blue theme, looks to be final release.

--- index.pl
+++ index.pl
@@ -1,5 +1,5 @@
-#!C:/xampp/perl/bin/perl.exe -w
 #!/usr/bin/perl
+#!C:/xampp/perl/bin/perl.exe -w
 
 use CGI qw(:cgi-lib :standard);
 use CGI::Carp qw(warningsToBrowser fatalsToBrowser);
@@ -32,7 +32,7 @@ Content-type: text/html
 <html>
 <head>
 	<meta charset="UTF-8" />
-	<title>Graderbot 9023</title>
+	<title>Gradebot 9023</title>
 	<link href='http://fonts.googleapis.com/css?family=Raleway|Montserrat' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="style.css">
 	
